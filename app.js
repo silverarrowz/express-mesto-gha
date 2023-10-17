fe4e148cc2755f9810672282669ff3cb9d581c6c
Dian4ik <jakie16@yandex.ru>
2023-10-17 21:12:18 +0800

    13pr User Router added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRouter = require('./routes/users');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_PATH = 'mongodb://localhost:27017/mestodb' } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect(DB_PATH, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+app.use(userRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ controllers/users.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+
+module.exports.getAllUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ error: err.message }));
+};
+
+module.exports.getUserById = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => res.status(500).send({ error: err.message }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send({ data: user }))
+    .catch((err) => res.status(400).send({ error: err.message }));
+};
