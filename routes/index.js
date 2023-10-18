fee04abf65a2f04df752c53e9749aa4bcbe48718
Dian4ik <jakie16@yandex.ru>
2023-10-18 23:09:09 +0800

    13pr autotests fixes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
-router.use('/', (req, res, next) => {
+router.use('/*', (req, res, next) => {
   next(res.status(404).send({ message: 'Переданы некорректные данные' }));
 });
 
