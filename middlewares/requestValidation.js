32b0e1213d25b9c3bb40be8347f6db8358271649
Dian4ik <jakie16@yandex.ru>
2023-10-25 21:21:34 +0800

    14пр проверка автотестов

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi, Segments } = require('celebrate');
 
-const regexUrl = /(\b(https?):\/\/(www\.)?(\w[a-zA-Z0-9-]{2,})+(\.{1})([a-z]{2,6})+)(\/[a-zA-Z0-9.\-_~:/?#[\]@!$&'()*+,;=]*)*/;
+const regexUrl = /\b(https?):\/\/(www\.)?[-A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]+\.([a-z]{2,6})+(\/[-A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]*)*/;
 
 // User data validation
 
