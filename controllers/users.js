f2807f1cf0988e8ddbf46de6719c1cc89f149dc5
Dian4ik <jakie16@yandex.ru>
2023-10-25 20:23:55 +0800

    14пр проверка автотестов

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ const User = require('../models/user');
 module.exports.getAllUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(next);
+    .catch((err) => next(err));
 };
 
 module.exports.getUserById = (req, res, next) => {
