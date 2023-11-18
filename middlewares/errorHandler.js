3fa0998d8c64cbbb0885e5c75216c6e072c969ae
Dian4ik <jakie16@yandex.ru>
2023-11-18 21:44:06 +0800

    15pr test commit

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Ошибка сервера';
+  const message = err.message || 'Ошибка на стороне сервера';
 
   console.error(`Ошибка: ${message}`);
   res.status(statusCode).send({ message });
